05186c72cbdda87a62a526685a4da9889e8eb432
ghost-cat <617570556@qq.com>
2017-03-02 22:46:30 +0800

    加入百度站点管理

--- resources/views/frontend/layout/head.blade.php
+++ resources/views/frontend/layout/head.blade.php
@@ -1,8 +1,9 @@
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="baidu-site-verification" content="MRvWiMwJrZ" />
+    <meta name="keywords" Content="永晨地板,地板,装修" />
+    <meta name="description" content="永晨地板是浙江浔工枋家居有限公司旗下一品牌，多年来致力于为全球消费者提供品质卓越、时尚领先的地板产品">
     <title>永晨地板</title>
-    
     <link href="/assets/frontend/css/bootstrap.css" rel='stylesheet' type='text/css' />
     <link href="/assets/frontend/css/owl.carousel.css" rel="stylesheet">
     <link href="/assets/frontend/css/style.css" rel="stylesheet" type="text/css" media="all" />
